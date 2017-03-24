ddf50ed94d2824c7e7f1f25957adb6aa1bd6015f
ahorod <prozerpina112009@gmail.com>
2017-03-24 10:28:35 -0700

    add result div's

--- js/scripts.js
+++ js/scripts.js
@@ -8,27 +8,32 @@ $(document).ready(function() {
   }
 });
   $("#form").submit(function(event) {
+    var frontbackendInput = $("#front-back-end").val();
+    var designInput = $("input:radio[name=focus]:checked").val();
     var companysizeInput = $("#companysize").val();
     var webappsInput = $("input:radio[name=ruby]:checked").val();
     var typeofsoftwareInput = $("#type-of-software").val();
 
-
-    if (ratingInput >= 0 && ratingInput <= 10) {
-      var scale = ratingInput;
-      if (animalInput === "Donkey") {
-        scale -= 3;
-      } else {
-        scale += 3;
-      }
-      if (regionInput === "coast") {
-        scale -= 3;
-      } else {
-        scale += 3;
-      }
-      $("#result").text(scale);
-    } else {
-      $("div.must-be-number").addClass("has-error");
+    if (frontbackendInput === "Front-end") {
+      $("#design-course").show();
     }
+    else if (frontbackendInput === "Back-end" && webappsInput === "yes") {
+      $("#ruby").show();
+    }
+    //   if (animalInput === "Donkey") {
+    //     scale -= 3;
+    //   } else {
+    //     scale += 3;
+    //   }
+    //   if (regionInput === "coast") {
+    //     scale -= 3;
+    //   } else {
+    //     scale += 3;
+    //   }
+    //   $("#result").text(scale);
+    // } else {
+    //   $("div.must-be-number").addClass("has-error");
+    // }
     event.preventDefault();
   });
 });
