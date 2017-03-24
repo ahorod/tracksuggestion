1c3b03c590f2704ac0e897915708fd7b9045e1b9
ahorod <prozerpina112009@gmail.com>
2017-03-24 10:01:15 -0700

    add variables for inputs

--- js/scripts.js
+++ js/scripts.js
@@ -7,27 +7,28 @@ $(document).ready(function() {
     $("#design").hide();
   }
 });
-  // $("#form").submit(function(event) {
-  //   var ratingInput = parseInt($("#rating").val());
-  //   var animalInput = $("#animal").val();
-  //   var regionInput = $("input:radio[name=region]:checked").val();
-  //
-  //   if (ratingInput >= 0 && ratingInput <= 10) {
-  //     var scale = ratingInput;
-  //     if (animalInput === "Donkey") {
-  //       scale -= 3;
-  //     } else {
-  //       scale += 3;
-  //     }
-  //     if (regionInput === "coast") {
-  //       scale -= 3;
-  //     } else {
-  //       scale += 3;
-  //     }
-  //     $("#result").text(scale);
-  //   } else {
-  //     $("div.must-be-number").addClass("has-error");
-  //   }
-  //   event.preventDefault();
-  // });
+  $("#form").submit(function(event) {
+    var companysizeInput = $("#companysize").val();
+    var webappsInput = $("input:radio[name=ruby]:checked").val();
+    var typeofsoftwareInput = $("#type-of-software").val();
+
+
+    if (ratingInput >= 0 && ratingInput <= 10) {
+      var scale = ratingInput;
+      if (animalInput === "Donkey") {
+        scale -= 3;
+      } else {
+        scale += 3;
+      }
+      if (regionInput === "coast") {
+        scale -= 3;
+      } else {
+        scale += 3;
+      }
+      $("#result").text(scale);
+    } else {
+      $("div.must-be-number").addClass("has-error");
+    }
+    event.preventDefault();
+  });
 });
